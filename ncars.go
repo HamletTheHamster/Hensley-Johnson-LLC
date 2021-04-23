26329bc39efbf0cdead1b2004542a4e257bf3e57
HamletTheHamster <joel.johnson675@gmail.com>
2021-04-23 12:59:34 -0700

    Added months from now calculation.

--- ncars.go
+++ ncars.go
@@ -5,9 +5,14 @@ import (
 
 func main() {
 
+  fmt.Printf("\n\n")
+  fmt.Printf("  -----------------\n")
+  fmt.Printf(" | nCar Calculator |        by Joel\n")
+  fmt.Printf("  -----------------\n")
+
   // 1 Car
 
-  fmt.Printf("Cost of car 1: ")
+  fmt.Printf("\n\nCost of car 1: ")
   var car1Cost float64
   fmt.Scanf("%f", &car1Cost)
 
@@ -15,8 +20,11 @@ func main() {
   var car1MonthlyRevenue float64
   fmt.Scanf("%f", &car1MonthlyRevenue)
 
-  fmt.Printf("Cost of car 1 is recovered after %.1f months.",
-    car1Cost/car1MonthlyRevenue)
+  monthsFromNow := car1Cost/car1MonthlyRevenue;
+
+  fmt.Printf("Cost of car 1 is recovered after %.1f months, ",
+    monthsFromNow)
+  fmt.Printf("or %.1f months from now.", monthsFromNow)
 
   // n Cars -> n+1 Cars
 
@@ -33,18 +41,32 @@ func main() {
     fmt.Printf("\n\nCost of car %d: ", n+1)
     fmt.Scanf("%f", &nextCarCost)
 
-    fmt.Printf("Existing car revenue alone will buy car %d after %.1f months.\n\n",
+    fmt.Printf("Existing car revenue alone will buy car %d after %.1f months, ",
       n+1, nextCarCost/nCarMonthlyRevenue)
 
-    fmt.Printf("Outside contributions toward purchase of car %d: ", n+1)
+    fmt.Printf("or %.1f months from now.",
+      monthsFromNow + nextCarCost/nCarMonthlyRevenue)
+
+    fmt.Printf("\n\nOutside contributions toward purchase of car %d, if any: ",
+      n+1)
     fmt.Scanf("%f", &nextCarOutsideContributions)
 
-    fmt.Printf("With this, car %d can be purchased after %.1f months\n\n",
-      n+1, (nextCarCost - nextCarOutsideContributions)/nCarMonthlyRevenue)
+    if nextCarOutsideContributions != 0 {
+      fmt.Printf("With this, car %d can be purchased after %.1f months, ",
+        n+1, (nextCarCost - nextCarOutsideContributions)/nCarMonthlyRevenue)
+
+      monthsFromNow -= nextCarCost/nCarMonthlyRevenue
+      monthsFromNow += (nextCarCost - nextCarOutsideContributions)/nCarMonthlyRevenue
+
+      fmt.Printf("or %.1f months from now.", monthsFromNow)
+
+    } else {
+      monthsFromNow += nextCarCost/nCarMonthlyRevenue
+    }
 
     // n+1 Cars
 
-    fmt.Printf("Monthly revenue of car %d: ", n+1)
+    fmt.Printf("\n\nMonthly revenue of car %d: ", n+1)
     fmt.Scanf("%f", &nextCarMonthlyRevenue)
 
     totalMonthlyRevenue := nCarMonthlyRevenue + nextCarMonthlyRevenue;
@@ -52,10 +74,14 @@ func main() {
     fmt.Printf("Total monthly revenue is now %.2f\n\n",
       totalMonthlyRevenue)
 
-    fmt.Printf("Cost of car %d is recovered after %.1f months.\n\n",
+    fmt.Printf("Cost of car %d is recovered after %.1f months, ",
       n+1, nextCarCost/totalMonthlyRevenue)
 
-    fmt.Printf("Buying another car? (true/false) ")
+    monthsFromNow += nextCarCost/totalMonthlyRevenue
+
+    fmt.Printf("or %.1f months from now.", monthsFromNow)
+
+    fmt.Printf("\n\nBuying another car? (true/false) ")
     fmt.Scanf("%t\n", &anotherCar)
 
     n++
@@ -67,13 +93,13 @@ func main() {
 
   }
 
-  fmt.Printf("--------------------------------------------------------------\n\n")
+  fmt.Printf("--------------------------------------------------------------")
 
-  fmt.Printf("\n\n Revenue per year with these %d cars: %.2f",
+  fmt.Printf("\n\n\n\nRevenue per year with these %d cars: %.2f",
     n, nCarMonthlyRevenue*12)
 
-  fmt.Printf("\n Total after 2 years: %.2f", nCarMonthlyRevenue*12*2)
-  fmt.Printf("\n Total after 3 years: %.2f", nCarMonthlyRevenue*12*3)
-  fmt.Printf("\n Total after 4 years: %.2f", nCarMonthlyRevenue*12*4)
+  fmt.Printf("\nTotal after 2 years: %.2f", nCarMonthlyRevenue*12*2)
+  fmt.Printf("\nTotal after 3 years: %.2f", nCarMonthlyRevenue*12*3)
+  fmt.Printf("\nTotal after 4 years: %.2f", nCarMonthlyRevenue*12*4)
 
 }
